31e0e32b8dcf6120cf567bb1476d5fb5f5ba1b31
smian1 <smian1@gmail.com>
2023-11-14 22:25:57 -0800

    Update voice_transcription.py
    Fixed errors with the keyboard listener.

--- voice_transcription.py
+++ voice_transcription.py
@@ -11,62 +11,69 @@ through keyboard shortcuts, specifically using Ctrl+R to start and stop recordin
 """
 
 
+#using API key file
 import pyaudio
 import wave
 import threading
 from pynput import keyboard
-from openai import OpenAI  # Used for accessing OpenAI's transcription services
-import pyperclip  # Handles copying text to the clipboard
-import pyautogui  # Automates keyboard and mouse operations
+from openai import OpenAI  # Importing OpenAI for transcription
+import pyperclip  # Import pyperclip
+import pyautogui  # allows you to programmatically control the mouse and keyboard.
 import os
-import datetime  # Used for timestamping events
+#import datetime  # Used for timestamping events
 
-# Path to the file containing the OpenAI API key
+# Define the path to the API key file
 API_KEY_FILE = 'openai_api_key.txt'
 
 def get_api_key():
-    # Check if the API key file exists and read the API key from it
+    # Check if the API key file exists in the current directory
     if os.path.isfile(API_KEY_FILE):
         with open(API_KEY_FILE, 'r') as file:
-            return file.read().strip()
+            api_key = file.read().strip()
+            #print(f"API Key: {api_key}")  # Temporary print statement for debugging
+            return api_key
     else:
-        raise FileNotFoundError(f"API key file not found in '{API_KEY_FILE}'.")
+        raise FileNotFoundError(f"API key file not found in the current directory. Please create a file named '{API_KEY_FILE}' with your OpenAI API key.")
 
-# Audio recording parameters
+
+# Set the recording parameters
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 CHUNK = 1024
 FILE_NAME = "recording.wav"
 
-# Variables to control the recording state
+# Global variables to control recording state
 is_recording = False
 recording_thread = None
-ctrl_pressed = False
+ctrl_pressed = False  # To track the state of Ctrl key
 
-# Initialize the PyAudio object
+# Initialize PyAudio
 audio = pyaudio.PyAudio()
 
+# Function to start recording
 def start_recording():
-    """Starts the audio recording."""
+    global paste_done
+    paste_done = False  # Reset the flag when starting a new recording
     global is_recording
     is_recording = True
-    print(f"{datetime.datetime.now()}: Recording started")
+    print("Recording started")
 
-    # Open the audio stream and start recording
-    stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,
+                        frames_per_buffer=CHUNK)
+
     frames = []
 
-    # Record audio in chunks until recording is stopped
     while is_recording:
         data = stream.read(CHUNK)
         frames.append(data)
 
-    # Stop and close the audio stream
+    # Stop and close the stream
     stream.stop_stream()
     stream.close()
 
-    # Save the recorded audio to a WAV file
+    # Save the recording
     wave_file = wave.open(FILE_NAME, 'wb')
     wave_file.setnchannels(CHANNELS)
     wave_file.setsampwidth(audio.get_sample_size(FORMAT))
@@ -74,85 +81,76 @@ def start_recording():
     wave_file.writeframes(b''.join(frames))
     wave_file.close()
 
-    # Transcribe the recorded audio
+    # After recording, transcribe the audio
     transcribe_audio()
 
 def transcribe_audio():
-    """Transcribes the recorded audio and handles the clipboard and pasting."""
+    print("Transcribe audio called")  # Debug print
     global paste_done
-    paste_done = False  # Reset the flag for each transcription
-    print(f"{datetime.datetime.now()}: Transcription started")
-    
     api_key = get_api_key()
     client = OpenAI(api_key=api_key)
-
-    # Open the recorded audio file and send it for transcription
     with open(FILE_NAME, "rb") as audio_file:
-        transcript_response = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
-
-    # Handle the transcription response
+        transcript_response = client.audio.transcriptions.create(
+            model="whisper-1", 
+            file=audio_file
+        )
+    
     if hasattr(transcript_response, 'text'):
         transcript = transcript_response.text
         print(transcript)
-        pyperclip.copy(transcript)  # Copy the transcript to the clipboard
-        print(f"{datetime.datetime.now()}: Transcription copied to clipboard.")
-        
-        # Paste the transcript if not already done
-        if not paste_done:
+        pyperclip.copy(transcript)  # Copy to clipboard
+        print("Transcription copied to clipboard.")
+        if not paste_done:  # Check if paste hasn't been done yet
+            #print("Clipboard content before paste:", pyperclip.paste())  # Debug print
             pyautogui.hotkey('command', 'v')  # Simulate Cmd+V to paste
-            paste_done = True
-            print(f"{datetime.datetime.now()}: Transcription pasted.")
+            paste_done = True  # Set the flag to True after pasting
     else:
         print("Transcription failed or the response format is not as expected.")
 
+# Function to stop recording
 def stop_recording():
-    """Stops the audio recording."""
     global is_recording
     is_recording = False
-    print(f"{datetime.datetime.now()}: Recording stopped")
+    print("Recording stopped")
 
+# Toggle recording state
 def toggle_recording():
-    """Toggles the recording state."""
     global is_recording, recording_thread
-    # Start or stop recording based on the current state
+    #print(f"Active Threads before toggling: {threading.active_count()}")  # New line
+    
     if is_recording:
         stop_recording()
     else:
         recording_thread = threading.Thread(target=start_recording)
-        recording_thread.daemon = True  # Run thread as a daemon
+        recording_thread.daemon = True  # Set thread as daemon
         recording_thread.start()
 
-# Keyboard event handlers
+# Listener for keyboard events
 def on_press(key):
-    """Handles key press events."""
     global ctrl_pressed
     try:
-        # Check if either Ctrl key is pressed
-        if key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+        if key == keyboard.Key.ctrl:
             ctrl_pressed = True
-    except AttributeError:
-        pass
-
-def on_release(key):
-    """Handles key release events."""
-    global ctrl_pressed
-    try:
-        # Check for Ctrl+R key combination
-        if key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
-            ctrl_pressed = False
+            #print("Ctrl pressed")  # Debug print
         elif key.char == 'r' and ctrl_pressed:
+            print("Ctrl+R pressed")  # Debug print
             toggle_recording()
     except AttributeError:
         pass
 
+def on_release(key):
+    global ctrl_pressed
+    if key == keyboard.Key.ctrl:
+        ctrl_pressed = False
+        #print("Ctrl released")  # Debug print
+
+
+# Main function
 def main():
-    """Main function to set up the keyboard listener."""
     try:
-        # Start the keyboard listener
         with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
             listener.join()
     except KeyboardInterrupt:
-        # Handle script exit
         print("Exiting script...")
         stop_recording()
 
